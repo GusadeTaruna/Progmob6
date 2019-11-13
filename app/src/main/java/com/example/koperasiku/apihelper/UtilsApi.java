7b9896091cae6934eb740cf48348d8d2ab0d6eaa
randomize721 <anggadp91@gmail.com>
2019-11-14 07:48:56 +0800

    ganti server API ke Heroku

--- app/src/main/java/com/example/koperasiku/apihelper/UtilsApi.java
+++ app/src/main/java/com/example/koperasiku/apihelper/UtilsApi.java
@@ -3,7 +3,7 @@ package com.example.koperasiku.apihelper;
 public class UtilsApi {
 
     // 10.0.2.2 ini adalah localhost.
-    public static final String BASE_URL_API = "https://progmob6api.000webhostapp.com/api/";
+    public static final String BASE_URL_API = "https://progmob6api.herokuapp.com/api/";
 
     // Mendeklarasikan Interface BaseApiService
     public static BaseApiService getAPIService(){
